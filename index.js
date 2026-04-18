4bfeb66872b9fe895b7f4c7b546368b69da1c95c
CatNap11Sans <sorvetequadrado@gmai.com>
2026-04-18 20:16:58 -0300

    primeiro commit - bot discord

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot online como ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('Pong! 🏓');
+  }
+});
+
+client.login(process.env.TOKEN);
+
+// Mantém o bot acordado no plano gratuito
+const http = require('http');
+http.createServer((req, res) => res.end('Bot online!')).listen(8080);
